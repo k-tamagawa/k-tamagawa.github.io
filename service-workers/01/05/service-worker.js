8bc991403392a86c8d19ac29cb44f22596efcebc
k_tamagawa <k_tamagawa@trancom.co.jp>
2021-01-15 10:42:16 +0900

    01/05の追加

--- service-workers/01/05/service-worker.js
+++ service-workers/01/05/service-worker.js
@@ -1,30 +1,36 @@
 var version = 1;
-var cacheName = 'stale- ' + version;
-self.addEventListener('install', function (event) {
-  self.skipWaiting();
+var cacheName = 'stale-' + version;
+
+self.addEventListener('install', function(event) {
+    self.skipWaiting();//wait状態にせずにすぐにactiveにする
 });
-self.addEventListener('activate', function (event) {
-  if (self.clients && clients.claim) {
-    clients.claim();
-  }
+
+self.addEventListener('activate', function(event) {
+    if (self.clients && clients.claim) {
+        clients.claim();//activeになったらすぐにコントロールする
+    }
 });
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    fetch(event.request).then(function (response) {
-      caches.open(cacheName).then(function (cache) {
-        if (response.status >= 500) {
-          cache.match(event.request).
-            then(function (response) {
-              return response;
-            }).catch(function () {
-              return response;
+
+self.addEventListener('fetch', function(event) {
+    // Always fetch response from the network
+    event.respondWith(
+        fetch(event.request).then(function(response) {
+            caches.open(cacheName).then(function(cache) {
+                // If we received an error response
+                if(response.status >= 500) {
+                    cache.match(event.request).then(function(response) {
+                        // Return stale version from cache
+                        return response;
+                    }).catch(function() {
+                        // No stale version in cache so return network response
+                        return response;
+                    });
+                } else {
+                    // Response was healthy so update cached version
+                    cache.put(event.request, response.clone());
+                    return response;
+                }
             });
-        } else {
-          cache.put(event.request,
-            response.clone());
-          return response;
-        }
-      });
-    })
-  );
+        })
+    );
 });
--- service-workers/memo.md
+++ service-workers/memo.md
@@ -1,6 +1,7 @@
 https://k-tamagawa.github.io/service-workers/01/01/index.html
 https://k-tamagawa.github.io/service-workers/01/02/index.html
 https://k-tamagawa.github.io/service-workers/01/03/index.html
+https://k-tamagawa.github.io/service-workers/01/05/index.html
 
 
 chrome://inspect/#service-workers:
