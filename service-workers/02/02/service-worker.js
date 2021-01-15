c01ec87e3bcd1c318d09315b39e2b26adaae602e
k_tamagawa <k_tamagawa@trancom.co.jp>
2021-01-15 13:15:34 +0900

    02/02 edit

--- service-workers/02/02/service-worker.js
+++ service-workers/02/02/service-worker.js
@@ -1,21 +1,28 @@
 var version = 1;
 var cacheName = 'static-' + version;
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll([
-              'index.html',
-              'packt-logo.png'
-            ]);
-        })
-    );
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll([
+        'index.html',
+        'packt-logo.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   //event.respondWith(caches.match(event.request));
-  event.respondWith(caches.match(event.request).then(function (response) {
-    return response;
-    })
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        } else {
+          //キャッシュされていない？？
+        };
+        return fetch(event.response);
+      })
   );
 });
