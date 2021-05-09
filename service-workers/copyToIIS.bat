f6aa91d717ce35e478d1830ea476bf1fdf210b65
k_tamagawa <k_tamagawa@trancom.co.jp>
2021-05-09 10:14:55 +0900

    フォルダごとにコピー

--- service-workers/copyToIIS.bat
+++ service-workers/copyToIIS.bat
@@ -1,1 +1,7 @@
-xcopy .\* C:\inetpub\wwwroot\service-workers /D /S /R /Y /I /K
+SET SW_PATH=C:\inetpub\wwwroot\service-workers
+xcopy .\01 %SW_PATH%\01 /D /S /R /Y /I /K
+xcopy .\02 %SW_PATH%\02 /D /S /R /Y /I /K
+xcopy .\03 %SW_PATH%\03 /D /S /R /Y /I /K
+xcopy .\04 %SW_PATH%\04 /D /S /R /Y /I /K
+xcopy .\05 %SW_PATH%\05 /D /S /R /Y /I /K
+xcopy .\08 %SW_PATH%\08 /D /S /R /Y /I /K
